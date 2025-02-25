644ce9f27b8fb4af8b73bd454bbb38ba8bc98eb2
Hasnain Arif <hasnainarif571@gmail.com>
2025-02-25 22:36:26 +0500

    Add Convex integration and Sonner for notifications; update styles and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_Georgian } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const defaultFont = Noto_Sans_Georgian({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Todovex.ai",
@@ -27,11 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className="className={defaultFont.className} antialiased">
+        <main>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </main>
+        <Toaster />
       </body>
     </html>
   );
